a6f2fca516cd1a828ae3ddb5b149b29ec4ce0843
Nguyen Van Dat <datsunbae@gmail.com>
2023-02-24 12:24:19 +0700

    Connect DB

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const {chats} = require('./data/data');
+const connectDB = require('./config/db');
 
 const app = express();
 
 dotenv.config();
+
+connectDB();
 
 app.get("/api/chat", (req, res) => {
     res.status(200).json(chats);
